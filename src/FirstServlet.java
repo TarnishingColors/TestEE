0982bebf88b7073e027b1857bfc718f63ba7f78d
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-08-29 23:33:09 +0300

    added annotations to servlets

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,6 +1,8 @@
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/hello-world")
 public class FirstServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
--- src/ThirdServlet.java
+++ src/ThirdServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ThirdServlet")
+@WebServlet("/3rd")
 public class ThirdServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
