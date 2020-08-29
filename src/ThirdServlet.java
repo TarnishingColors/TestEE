0f07e648068c9d51af71b865befa92adb7a49eb8
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-08-30 00:38:45 +0300

    implemented work with session

--- src/ThirdServlet.java
+++ src/ThirdServlet.java
@@ -3,7 +3,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/3rd")
 public class ThirdServlet extends HttpServlet {
@@ -13,7 +15,19 @@ public class ThirdServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.sendRedirect("/hello-world");
+        HttpSession session = request.getSession();
 
+        Integer counter = (Integer) session.getAttribute("count");
+
+        if (counter == null) {
+            counter = 0;
+        }
+
+        session.setAttribute("count", counter + 1);
+
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> Hi, visitor </h1");
+        pw.println("<p> you visit this page for " + counter + " time </p>");
     }
 }
