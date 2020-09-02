1088680c9b688ae20d31c39e525aa7860e8dac4f
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-09-02 15:18:55 +0300

    trying to access page with threads

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -1,9 +1,12 @@
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
 @WebServlet("/hello-world")
-public class FirstServlet extends javax.servlet.http.HttpServlet {
+public class FirstServlet extends HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
@@ -20,6 +23,23 @@ public class FirstServlet extends javax.servlet.http.HttpServlet {
         } else {
             pw.println("<p> Hello, " + name + "</p>");
         }
+        pw.println("<p>" + Thread.currentThread().getName() + "</p>");
         pw.println("</html");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    URLConnection urlConnection =
+                            new URL("http://localhost:8080/hello-world").openConnection();
+                    urlConnection.getInputStream();
+                    System.out.println(Thread.currentThread().getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+        }
     }
 }
