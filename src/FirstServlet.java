fdbac1ca2e389ce65029450d620f81a22f7a74a8
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-09-03 13:27:19 +0300

    implemented form

--- src/FirstServlet.java
+++ src/FirstServlet.java
@@ -8,22 +8,33 @@ import java.net.URLConnection;
 @WebServlet("/hello-world")
 public class FirstServlet extends HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
         String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
         PrintWriter pw = response.getWriter();
 
         pw.println("<html>");
         pw.println("<h1> Kentrosaurus </h1>");
+        pw.println("<body");
+        pw.println("<p> name: " + name + "</p>");
+        pw.println("<form action='hello-world' method='post'/>");
+        pw.println("<input type='text' name='name'/>");
+        pw.println("<input type='text' name='surname'>");
+        pw.println("<input type='submit' name='submit'/>");
+        pw.println("</form>");
+        pw.println("</body>");
         if (name == null) {
             pw.println("<p> Hello, unknown master </p>");
         } else {
             pw.println("<p> Hello, " + name + "</p>");
         }
         pw.println("<p>" + Thread.currentThread().getName() + "</p>");
+        pw.println("</body");
         pw.println("</html");
     }
 
